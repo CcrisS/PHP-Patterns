c355b1ded8c27e4b0e1500add7e1b7d7ae0735a4
Cristina Sanz <cristina.sanz@colaboradoresunidadeditorial.es>
2015-09-29 16:24:58 +0200

    factory dessign pattern

--- creational/factory/Client.php
+++ creational/factory/Client.php
@@ -1,27 +1,18 @@
 <?php
-/* http://cristina.sanz.tidwell.dev.internet.int/uploads/other_projects/others/php_patterns/creational/factory/Client.php */
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//include_once ('GraphicFactory.php'); // 1
-//include_once ('TextFactory.php'); // 1
 include_once('CountryFactory.php');
 include_once('KyrgyzstanProduct.php');
 include_once('MoldovaProduct.php');
 
 class Client {
 
-    // private $someGraphicObject; //1
-    // private $someTextObject; //1
     private $countryFactory;
     
-    public function __construct() {
-        // $this->someGraphicObject = new GraphicFactory(); //1
-        // echo $this->someGraphicObject->startFactory() . '<br>'; //1
-        // $this->someTextObject    = new TextFactory(); //1
-        // echo $this->someTextObject->startFactory() . '<br>'; //1
-
+    public function __construct() 
+    {
         $this->countryFactory = new CountryFactory();
         echo $this->countryFactory->doFactory(new KyrgyzstanProduct());
         echo '<br><hr><br>';
